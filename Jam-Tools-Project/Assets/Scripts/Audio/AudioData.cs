04f8003d3130ce4bb68cee4396ce2bc1e4e7d3d1
GishaTheDeveloper <spaceshhz@gmail.com>
2020-11-03 22:50:17 +0200

    Init Commit

---
+++ Jam-Tools-Project/Assets/Scripts/Audio/AudioData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AudioData
+{
+    [Header("General")]
+    public string Name;
+    public AudioClip audioClip;
+
+    [Header("Audio Source Settings")]
+    public bool isLooping;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.3f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public GameObject go;
+    [HideInInspector]
+    public AudioSource audioSource;
+}
